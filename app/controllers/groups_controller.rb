d04cffe5abc204b2e085dabcb50b268a305c1134
Ivan Stroganov <knowhere@gmail.com>
2014-03-27 14:39:15 -0700

    group crud

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,16 +1,45 @@
 class GroupsController < ApplicationController
+
+  def new
+    @group = Group.new
+  end
+
   def create
+    group = Group.new group_params
+
+    if group.save
+      redirect_to :groups
+    else
+      render :new
+    end
   end
 
   def index
+    @groups = Group.find :all, order: 'id'
   end
 
   def show
   end
 
-  def update
+  def edit
+    @group = Group.find params[:id]
   end
 
-  def delete
+  def update
+    @group = Group.find params[:id]
+    @group.update group_params
+    redirect_to :groups
   end
+
+  def destroy
+    Group.destroy params[:id]
+    redirect_to :groups
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :active)
+  end
+
 end
