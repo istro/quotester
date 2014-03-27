0a12a0f4a2c07866d0d14c9a16d1e8011e8f90ca
Ivan Stroganov <knowhere@gmail.com>
2014-03-27 11:15:31 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quotester::Application.config.secret_key_base = '96a5151e17e7757eed93253ea8261ce3e4267184cecf9e7710502ea3e90172d566b0aa20c6bd4a197cf5ebb9cb443e4db52256f5d18073c13dab7ed04c68db72'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Quotester::Application.config.session_store :cookie_store, key: '_quotester_session'
