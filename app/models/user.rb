aabd8c379e4f16c00571c051a14cd1aaea48d516
Ivan Stroganov <knowhere@gmail.com>
2014-03-28 15:07:34 -0700

    add devise, configure for username signin

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,22 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable,
+         :authentication_keys => [:login]
+  # removed :validatable per http://stackoverflow.com/a/17993407/1592915
+
+  # per devise readme http://goo.gl/6Ldiv
+  attr_accessor :login
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+
+
 end
